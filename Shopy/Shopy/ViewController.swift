3c156e45b331bd8fedf704d12e5712076354d1a9
Yevgeniy <yevgeniy.shin.v@gmail.com>
2022-11-09 07:59:52 +0300

    initial commit

---
+++ Shopy/Shopy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shopy
+//
+//  Created by boockich mac on 09.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
